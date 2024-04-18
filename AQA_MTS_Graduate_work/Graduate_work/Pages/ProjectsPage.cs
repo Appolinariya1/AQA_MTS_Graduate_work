216bb5e9672bae44d0197b21a38a631ac02f9a43
Appolinariya Koval <polinaholod97@gmail.com>
2024-04-19 00:20:43 +0300

    add DialogWindowTest

--- AQA_MTS_Graduate_work/Graduate_work/Pages/ProjectsPage.cs
+++ AQA_MTS_Graduate_work/Graduate_work/Pages/ProjectsPage.cs
@@ -8,6 +8,9 @@ public class ProjectsPage : BasePage
     
     // Описание элементов
     private static readonly By TitleLabelBy = By.XPath("//h1[text()='Projects']");
+    private static readonly By CreateProjectButtonBy = By.Id("createButton");
+    private static readonly By CreateProjectModalBy = By.XPath(
+        "//div[@class='ReactModalPortal']//div[@role='dialog']//h3[text()='Create new project']");
     
     public ProjectsPage(IWebDriver driver, bool openPageByUrl = false) : base(driver, openPageByUrl)
     {
@@ -25,4 +28,9 @@ public class ProjectsPage : BasePage
     
     // Методы
     public IWebElement TitleLabel => WaitsHelper.WaitForExists(TitleLabelBy);
+    public IWebElement CreateProjectButton => WaitsHelper.WaitForExists(CreateProjectButtonBy);
+    public IWebElement CreateProjectModal => WaitsHelper.WaitForExists(CreateProjectModalBy);
+    
+    //Методы действий
+    public void ClickCreateProjectButton() => CreateProjectButton.Click();
 }
--- AQA_MTS_Graduate_work/Graduate_work/Tests/GUI/DialogWindowTest.cs
+++ AQA_MTS_Graduate_work/Graduate_work/Tests/GUI/DialogWindowTest.cs
@@ -1,6 +1,25 @@
+using Graduate_work.Helpers.Configuration;
+using Graduate_work.Pages;
+using Graduate_work.Steps;
+
 namespace Graduate_work.Tests;
 
-public class DialogWindowTest
+public class DialogWindowTest : BaseGuiTest
 {
-    
+    private ProjectsPage _projectsPage;
+
+    [SetUp]
+    public void DialogWindowTestInit()
+    {
+        _projectsPage = NavigationSteps
+            .SuccessfulLogin(Configurator.Users.First(x => x?.Email == "polinaholod97@gmail.com"));
+    }
+
+    [Test]
+    public void DialogWindowCreateProjectTest()
+    {
+        _projectsPage.ClickCreateProjectButton();
+
+        Assert.That(_projectsPage.CreateProjectModal.Displayed);
+    }
 }
