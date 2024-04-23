e933a7d550730002e86c9d2a28ef9a252b86b849
Appolinariya Koval <polinaholod97@gmail.com>
2024-04-23 23:11:43 +0300

    add restclient and token authentification

--- AQA_MTS_Graduate_work/Graduate_work/Tests/API/BaseApiTest.cs
+++ AQA_MTS_Graduate_work/Graduate_work/Tests/API/BaseApiTest.cs
@@ -1,12 +1,20 @@
+using Graduate_work.Clients;
 using Graduate_work.Steps;
+using NLog;
+using NUnit.Allure.Core;
 using OpenQA.Selenium;
 
 namespace Graduate_work.Tests.API;
 
+[Parallelizable(scope: ParallelScope.Fixtures)]
+[AllureNUnit]
 public class BaseApiTest
 {
-    protected IWebDriver Driver { get; private set; }
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-    protected NavigationSteps _navigationSteps;
-    
+    [OneTimeSetUp]
+    public void SetUpApi()
+    {
+        var restClient = new RestClientExtended();
+    }
 }
